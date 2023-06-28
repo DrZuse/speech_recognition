4d32ac85697a363aa0fa481d4ff1ee9e988e9364
DrZuse <53859830+DrZuse@users.noreply.github.com>
2023-06-28 08:53:39 +0300

    google speech recognition

---
+++ 1.py
@@ -0,0 +1,17 @@
+# google speech recognition
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+file_path = "swartz.wav"
+
+with sr.AudioFile(file_path) as source:
+    audio = r.record(source)
+
+try:
+    text = r.recognize_google(audio)
+    print("You said:", text)
+except sr.UnknownValueError:
+    print("Could not understand audio.")
+except sr.RequestError as e:
+    print("Could not request results; {0}".format(e))
